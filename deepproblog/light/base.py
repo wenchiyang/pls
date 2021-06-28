114ede8d41c6c58fb8d1cd0cb8e5e98e85e8d4b0
giuseppe <giuseppe.marra.91@gmail.com>
2021-06-28 17:35:46 +0200

    Adding DPL policy

---
+++ deepproblog/light/base.py
@@ -0,0 +1,42 @@
+from torch import nn
+import torch
+from problog.formula import LogicFormula, LogicDAG
+from problog.sdd_formula import SDD
+from deepproblog.light.semiring import GraphSemiring
+from collections import defaultdict
+
+class DeepProbLogLayer(nn.Module):
+
+    def __init__(self, program, queries="", evidence="", single_output = None):
+        super().__init__()
+        query = "\n".join(["query(%s)."% q for q in queries])
+        self.program = "\n\n".join([program,evidence,query])
+        self.lf = LogicFormula.create_from(self.program)
+        self.dag = LogicDAG.create_from(self.lf)
+        self.sdd = SDD.create_from(self.dag)
+        self.semiring = GraphSemiring()
+        self.single_output = single_output
+
+    def forward(self, x):
+        """x is a dictionary <key,tensor>."""
+        if self.single_output is not None:
+            self.semiring.set_weights({self.single_output: x})
+        else:
+            self.semiring.set_weights(x)
+        out = self.sdd.evaluate(semiring = self.semiring)
+        stacked = defaultdict(list)
+        for k,v in out.items():
+            stacked[k.functor].append((k,v))
+
+        tensorial = {}
+        for k,v in stacked.items():
+            v = [b for a,b in sorted(v, key=lambda x:str(x[0]))]
+            tensorial[k] = torch.cat(v, dim=-1)
+
+        if self.single_output:
+            return tensorial[self.single_output]
+        else:
+            return tensorial
+
+
+
