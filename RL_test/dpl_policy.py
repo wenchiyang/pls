9ba15ee0faaacb14566dbed80c33f9431540ec9c
giuseppe <giuseppe.marra.91@gmail.com>
2021-06-29 09:33:04 +0200

    Adding DPL policy

--- RL_test/dpl_policy.py
+++ RL_test/dpl_policy.py
@@ -9,29 +9,20 @@ program = """
 wall(X,Y) :- (X<0;X>1;Y<0;Y>1).
 
 
-action(0)::action(stay); 
-action(1)::action(up); 
-action(2)::action(down); 
-action(3)::action(left); 
-action(4)::action(right).
+action(0)::action(stay); action(1)::action(up); action(2)::action(down); 
+action(3)::action(left); action(4)::action(right).
 
 
 % Ghost and Pacman positions are mutually exclusive for now (i.e. only one ghost and one pacman).
-ghost(0)::ghost(0,0);
-ghost(1)::ghost(0,1);
-ghost(2)::ghost(1,0);
-ghost(3)::ghost(1,1).
+ghost(0)::ghost(0,0);ghost(1)::ghost(0,1);ghost(2)::ghost(1,0);ghost(3)::ghost(1,1).
 
-pacman(0)::pacman(0,0);
-pacman(1)::pacman(0,1);
-pacman(2)::pacman(1,0);
-pacman(3)::pacman(1,1).
+pacman(0)::pacman(0,0);pacman(1)::pacman(0,1);pacman(2)::pacman(1,0);pacman(3)::pacman(1,1).
 
 
 % Transition
-transition(X,Y,west,X1,Y) :- X1 is X - 1.
-transition(X,Y,east,X1,Y) :- X1 is X + 1.
 transition(X,Y,stay,X,Y).
+transition(X,Y,left,X1,Y) :- X1 is X - 1.
+transition(X,Y,right,X1,Y) :- X1 is X + 1.
 transition(X,Y,up,X,Y1) :- Y1 is Y + 1.
 transition(X,Y,down,X,Y1) :- Y1 is Y - 1.
 
@@ -75,9 +66,6 @@ class DPLSafePolicy(nn.Module):
         pacman = self.pacman_layer(h)
         base_actions = self.base_policy_layer(h)
 
-
-        # Map from functors to the probability of their groundings.
-        # TODO(giuseppe): We need to fix an order here
         weights = {"ghost": ghosts,
                    "pacman": pacman,
                    "action": base_actions}
