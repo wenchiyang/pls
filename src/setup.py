f02a66c817874901b1489e425d47e713c4ccc8e6
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2021-07-26 11:14:26 +0800

    Add src package

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="gradient_shielding",
+    version="0.0.1",
+    install_requires=[
+        "gym",
+        "torch",
+        "cherry-rl",
+        "altair",
+    ],  # And any other dependencies required
+)
