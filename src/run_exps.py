bdf23dee2ac7bae5022c908ce1c492a15753a8d0
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2022-04-14 21:18:34 +0200

    Fix exps

--- src/run_exps.py
+++ src/run_exps.py
@@ -70,8 +70,8 @@ if __name__ == "__main__":
                 #  "alpha_0.7", "alpha_0.9",
                 "vsrl"],
         "seeds":
-            ["seed1", "seed2", "seed3", "seed4", "seed5"]
-            # ["seed1"]
+            # ["seed1", "seed2", "seed3", "seed4", "seed5"]
+            ["seed1"]
     }
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
