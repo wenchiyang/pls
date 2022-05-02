6bc755e841be4a0c81127a1225e1e52402d68cc7
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2022-05-02 20:12:18 +0200

    Add exps

--- src/run_exps.py
+++ src/run_exps.py
@@ -62,17 +62,20 @@ if __name__ == "__main__":
         "exp_folders": ["experiments_trials3"],
         "domains": [
             "sokoban/2box10map",
-            "goal_finding/smallGrid100map"
+            # "goal_finding/smallGrid100map",
+            "goal_finding/7grid5g"
+            # "sokoban/3box10map"
         ],
         "exps": [
-                "no_shielding",
-                "hard_shielding",
-                 "alpha_0.1",
-                 "alpha_0.3",
-                 "alpha_0.5",
-                 "alpha_0.7",
-                 "alpha_0.9",
-                "vsrl"
+                # "no_shielding",
+                # "hard_shielding",
+                #  "alpha_0.1",
+                #  "alpha_0.3",
+                #  "alpha_0.5",
+                #  "alpha_0.7",
+                #  "alpha_0.9",
+                "alpha_learned"
+                # "vsrl"
             ],
         "seeds":
             ["seed1", "seed2", "seed3", "seed4", "seed5"]
