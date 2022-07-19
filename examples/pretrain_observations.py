e8da4fbf852563b2542dda132adaa30216beff2b
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2022-07-19 15:51:28 +0200

    pretraining sokoban

--- examples/pretrain_observations.py
+++ examples/pretrain_observations.py
@@ -1,7 +1,7 @@
 import os
 from pls.workflows.pre_train import generate_random_images_gf, generate_random_images_sokoban
 from pls.workflows.execute_workflow import pretrain_observation_gf, pretrain_observation_sokoban
-
+from dask.distributed import Client
 
 def pre_train_gf():
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -22,7 +22,7 @@ def pre_train_gf():
     # # pretrain_observation(csv_file, img_folder, model_folder, 10000)
 
 
-def pre_train_sokoban():
+def pre_train_sokoban(n_train):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     img_folder = os.path.join(dir_path, "../pls/data/sokoban")
     csv_file = os.path.join(img_folder, "labels.csv")
@@ -30,14 +30,24 @@ def pre_train_sokoban():
     if not os.path.exists(img_folder):
         os.makedirs(img_folder)
 
-    # generate_random_images_sokoban(csv_file, img_folder, 10100)
+    generate_random_images_sokoban(csv_file, img_folder, 10100)
 
     model_folder = os.path.join(dir_path, "../experiments_trials3/sokoban/2box5map_gray/data/")
     if not os.path.exists(model_folder):
         os.makedirs(model_folder)
 
-    pretrain_observation_sokoban(csv_file, img_folder, model_folder, 1000, 300)
-    pretrain_observation_sokoban(csv_file, img_folder, model_folder, 10000, 300)
+    # pretrain_observation_sokoban(csv_file, img_folder, model_folder, n_train, 300)
+    # pretrain_observation_sokoban(csv_file, img_folder, model_folder, n_train, 300)
+
+
+def main_cluster():
+    client = Client("134.58.41.100:8786")
+
+    # with performance_report(filename="dask-report.html"):
+    ## some dask computation
+    futures = client.map(pre_train_sokoban, [1000, 10000])
+    results = client.gather(futures)
 
 if __name__ == "__main__":
-    pre_train_sokoban()
+    main_cluster()
+
