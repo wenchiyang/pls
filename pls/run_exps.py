798ff907dd37826f7c579229ca1f10cbc0f43460
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2022-08-27 20:35:09 +0200

    clean GF

--- pls/run_exps.py
+++ pls/run_exps.py
@@ -62,8 +62,8 @@ if __name__ == "__main__":
         ],
         "exps": [
             "PPO",
-            "PLSperfect",
-            "VSRLperfect",
+            "PLSperf",
+            "VSRLperf",
             ],
         "seeds":
             # ["PPO", "PLS", "seed3", "seed4", "seed5"]
